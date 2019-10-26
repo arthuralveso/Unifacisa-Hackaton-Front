98efbfaa53d9fc2203536038a82d5940e35cf5c3
Sergio Davi <sergio.teotonio@ctis.com.br>
2019-10-26 19:40:28 -0300

    Ajustes

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -1,20 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    CommonModule,
-    HeaderComponent
+    CommonModule
   ]
 })
 export class SharedComponentsModule { }
